24313ef3e6e9f39b683e2bf08c20eeae80960fc8
matthanm <matthanmyers@gmail.com>
2016-12-02 03:14:26 -0600

    Final touches for 1.0

--- Server.py
+++ Server.py
@@ -1,18 +1,22 @@
 from flask import Flask, request
+import json
+import urllib.request
 import threading
 
 class Server(threading.Thread):
-	def __init__(self):
-		super(Server, self).__init__()
-		self.postQueue = []
-		self.app = Flask(__name__)
-		@self.app.route('/', methods=['POST'])
-		def result():
-			print("Received message.")
-			self.postQueue.append(request.form['text'])
-			return 'Received !'
-	def run(self):
-		print("Starting server...")
-		self.app.run(threaded=True, host="0.0.0.0")
-	def stop(self):
-		print("Stopping server...")
+    def __init__(self):
+        super(Server, self).__init__()
+        self.postQueue = []
+        self.app = Flask(__name__)
+        @self.app.route("/", methods=["POST"])
+        def result():
+            print("Received message.")
+            data = json.loads(request.data.decode("utf-8"))
+            if data["sender_type"] != "bot":
+                self.postQueue.append(data["text"])
+            return "Response !"
+    def run(self):
+        print("Starting server...")
+        self.app.run(threaded=True, host="0.0.0.0")
+    def stop(self):
+        print("Stopping server...")
